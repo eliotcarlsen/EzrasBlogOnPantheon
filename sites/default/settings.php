2eb3907f76565dc6dc769c15d6e6ee033f58835a
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-06-06 11:30:19 -0700

    add custom contact module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'eliotsdatabase',
+      'username' => 'eliot',
+      'password' => 'everest1',
+      'host' => 'localhost',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'cY0k24DTsz9YtwCbJUyU402if45HfcSGcw0wl3EVmtw';
 
 /**
  * Base URL (optional).
